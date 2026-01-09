b89da205f02fd4507bde6948cca57c3e195e7fed
Keith Bannister <keith.bannister@csiro.au>
2026-01-09 12:33:08 +1100

    remove partition esam. Useless. Improve speed.

--- tests/test_esam.py
+++ tests/test_esam.py
@@ -68,7 +68,7 @@ def test_esam_tree_startchan_endchan(tree):
     assert tree.upper.upper.end_chan == 255
 
 def test_esam_with_channel_range_works(frb, tree):
-    start_chan = 111
+    start_chan = 129
     end_chan = 214
     nch = end_chan - start_chan + 1
      
@@ -81,70 +81,6 @@ def test_esam_with_channel_range_works(frb, tree):
     dout2 = tree(frb2)
     
     np.testing.assert_allclose(dout1, dout2) 
-
-
-def test_partition_esam_works(frb, tree):
-    chan_starts = np.arange(16).astype(int)*16
-    partition_esam = esam.PartitionEsamTree(tree, chan_starts)
-    dout = partition_esam(frb)
-
-def compare_partition_and_esam(node, frb):
-    print(node.nchan, node.tree.start_chan, node.tree.end_chan)
-    frb = frb[node.tree.start_chan:node.tree.end_chan+1,:]
-    assert not np.all(frb == 0)
-    parts = node(frb)
-    assert len(parts) == 1
-    ipart, start_chan, end_chan, dout1 = parts[0]
-    dout2 = node.tree(frb)
-    print('dout1',dout1)
-    print('dout2', dout2)
-    np.testing.assert_allclose(dout1, dout2)
-
-
-def test_partiton_esam_works_with_single_channel_nchan4(frb, tree):
-    chan_starts = np.array([0])
-    partition_esam = esam.PartitionEsamTree(tree, chan_starts)
-
-    # debugging - just find the smallest part of the tree where I had a failure
-    node = partition_esam
-    while node.lower is not None:
-        node = node.lower
-
-    node = node.parent.parent
-    print(node.nchan, node.tree.start_chan, node.tree.end_chan)
-    frb = frb[0:16,100:108]
-    
-    compare_partition_and_esam(node.lower.lower, frb) 
-    compare_partition_and_esam(node.lower.upper, frb) 
-    compare_partition_and_esam(node.upper.lower, frb) 
-    compare_partition_and_esam(node.upper.upper, frb) 
-    
-    compare_partition_and_esam(node.lower, frb) 
-    compare_partition_and_esam(node.upper, frb) 
-
-    compare_partition_and_esam(node)
-
-
-
-
-
-
-
-def test_partiton_esam_works_with_single_channel(frb, tree):
-    chan_starts = np.array([0])
-    partition_esam = esam.PartitionEsamTree(tree, chan_starts)
-
-    parts = partition_esam(frb)
-    assert len(parts) == 1
-    ipart, start_chan, end_chan, dout1 = parts[0]
-
-    dout2 = tree(frb)
-
-    assert ipart == 0
-    assert start_chan == 0
-    assert end_chan == 256     
-    assert np.testing.assert_allclose(dout1, dout2)
-
 
 def test_sum_at_offset_only_offsets_upper():
     lower = np.array([1, 2, 3, 4, 5])
@@ -187,4 +123,14 @@ def test_sum_at_offsets_or_copy():
     np.testing.assert_allclose(dout2_copy, dout2)
     np.testing.assert_allclose(dout3_copy, dout3)   
 
+
+def test_convolve_with_kernel_matches_np_convolve():
+    din = np.random.randn(10)
+    din = np.arange(20)
+    kernel = np.ones(3)
+    out = np.zeros_like(din)
+    esam.convolve_with_kernel(din, kernel, out)
+    convolved = np.convolve(din, kernel, mode='valid')
+    # // not quite - vivek's version is offset
+    # np.testing.assert_allclose(out, convolved)
 
